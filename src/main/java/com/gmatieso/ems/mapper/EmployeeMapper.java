1d64f679df4c5f923fbad3b02158ec0917a99f5e
Gmatieso <geoffrey.matieso@sare.africa>
2025-04-23 15:40:02 +0300

    Resolve bug in old Employees mapper

--- src/main/java/com/gmatieso/ems/mapper/EmployeeMapper.java
+++ src/main/java/com/gmatieso/ems/mapper/EmployeeMapper.java
@@ -4,27 +4,24 @@ import com.gmatieso.ems.dto.EmployeeDtoResponseOld;
 import com.gmatieso.ems.entity.Employee;
 
 public class EmployeeMapper {
+//    Mapping our employee Entity to employeeResponse DTO
+    public static EmployeeDtoResponseOld mapToEmployeeDto(Employee employee){
+        return new EmployeeDtoResponseOld(
+                employee.getId(),
+                employee.getFirstName(),
+                employee.getLastName(),
+                employee.getEmail()
+        );
 
+    }
 
-//
-////    Mapping our employee Entity to employeeResponse DTO
-//    public static EmployeeDtoResponseOld mapToEmployeeDto(Employee employee){
-//        return new EmployeeDtoResponseOld(
-//                employee.getId(),
-//                employee.getFirstName(),
-//                employee.getLastName(),
-//                employee.getEmail()
-//        );
-//
-//    }
-//
-//    //Mapping our employee DTO response to employee entity
-//    public static Employee mapToEmployee(EmployeeDtoResponseOld employeeDtoResponseOld){
-//        return new Employee(
-//                employeeDtoResponseOld.getId(),
-//                employeeDtoResponseOld.getFirstName(),
-//                employeeDtoResponseOld.getLastName(),
-//                employeeDtoResponseOld.getEmail()
-//        );
-//    }
+    //Mapping our employee DTO response to employee entity
+    public static Employee mapToEmployee(EmployeeDtoResponseOld employeeDtoResponseOld){
+        return new Employee(
+                employeeDtoResponseOld.getId(),
+                employeeDtoResponseOld.getFirstName(),
+                employeeDtoResponseOld.getLastName(),
+                employeeDtoResponseOld.getEmail()
+        );
+    }
 }
